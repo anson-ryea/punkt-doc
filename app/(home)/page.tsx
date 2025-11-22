9335750c3852d1732a0ae43cf47624af178130d9
Anson Ng <914anson5@gmail.com>
2025-11-22 23:59:35 +0800

    [feat] redesign the home page layout and rename references to reference

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,16 +1,56 @@
-import Link from 'next/link';
+import Link from "next/link";
+import { Cards, Card } from "fumadocs-ui/components/card";
+import { SmileIcon, ToolCaseIcon, BookMarkedIcon } from "lucide-react";
 
 export default function HomePage() {
-  return (
-    <div className="flex flex-col justify-center text-center flex-1">
-      <h1 className="text-2xl font-bold mb-4">Hello World</h1>
-      <p>
-        You can open{' '}
-        <Link href="/docs" className="font-medium underline">
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
-    </div>
-  );
+    return (
+        <div className="flex flex-col justify-center text-center flex-1 space-y-8 px-32">
+            <div className="space-y-4">
+                <h1 className="text-4xl font-bold">
+                    Want to know more abount Punkt?
+                </h1>
+                <h4>Portal to different sections of documentation.</h4>
+            </div>
+            <div>
+                <Cards>
+                    <Card
+                        icon={<SmileIcon />}
+                        href="/docs/user-guide"
+                        title=""
+                        className="text-left"
+                    >
+                        <h1 className="text-base font-medium text-white">
+                            User Guide
+                        </h1>
+                        <p>Be a power user and master Punkt.</p>
+                    </Card>
+                    <Card
+                        icon={<ToolCaseIcon />}
+                        href="/docs/developer-guide"
+                        title=""
+                        className="text-left"
+                    >
+                        <h1 className="text-base font-medium text-white">
+                            Developer Guide
+                        </h1>
+                        <p>
+                            Everything you need to know before you start
+                            developing with Punkt.
+                        </p>
+                    </Card>
+                    <Card
+                        icon={<BookMarkedIcon />}
+                        href="/docs/references"
+                        title=""
+                        className="text-left"
+                    >
+                        <h1 className="text-base font-medium text-white">
+                            Reference
+                        </h1>
+                        <p>Punkt's commands and options in detail.</p>
+                    </Card>
+                </Cards>
+            </div>
+        </div>
+    );
 }
