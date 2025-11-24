fe8cc6b0226f9dec66c877a612d0ccb3516b8386
Anson Ng <914anson5@gmail.com>
2025-11-25 00:02:51 +0800

    [doc] add docs for ignoring files

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,11 +1,13 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import * as TabsComponents from "fumadocs-ui/components/tabs";
+import * as Twoslash from "fumadocs-twoslash/ui";
 import type { MDXComponents } from "mdx/types";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     ...TabsComponents,
+    ...Twoslash,
     ...components,
   };
 }
